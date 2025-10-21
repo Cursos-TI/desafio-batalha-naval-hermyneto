d79cd25d6cea9672bcbcd74c24f56b2786876aec
hermyneto <hermyneto@gmail.com>
2025-10-21 01:46:56 +0000

    Tema - 5 _ Nivel Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,45 +1,60 @@
 #include <stdio.h>
 
-int main(){
+#define TAMANHO 10
+#define NAVIO 3
+#define TAM_NAVIO 3
 
-    // Tabuleiro 10x10 inicializado com 0 (água)
-    int tabuleiro[10][10] = {0};
-    int navio = 3;     // Valor que representa o navio
-    int tam_navio = 3; // Tamanho do navio
+int main()
+{
+    int tabuleiro[TAMANHO][TAMANHO];
 
-    // Coordenadas iniciais dos navios
-    int linha_horizontal = 2;
-    int coluna_horizontal = 4;
-
-    int linha_vertical = 6;
-    int coluna_vertical = 3;
-
-    // Posiciona navio horizontal
-    for (int i = 0; i < tam_navio; i++)
+    // Inicializa o tabuleiro com água (0)
+    for (int i = 0; i < TAMANHO; i++)
     {
-        tabuleiro[linha_horizontal][coluna_horizontal + i] = navio;
+        for (int j = 0; j < TAMANHO; j++)
+        {
+            tabuleiro[i][j] = 0;
+        }
     }
 
-    // Posiciona navio vertical
-    for (int i = 0; i < tam_navio; i++)
+    // Navio horizontal em (7,7) até (7,9)
+    for (int i = 0; i < TAM_NAVIO; i++)
     {
-        tabuleiro[linha_vertical + i][coluna_vertical] = navio;
+        tabuleiro[7][7 + i] = NAVIO;
+    }
+
+    // Navio vertical em (3,4) até (5,4)
+    for (int i = 0; i < TAM_NAVIO; i++)
+    {
+        tabuleiro[3 + i][4] = NAVIO;
+    }
+
+    // Navio diagonal principal em (0,0) até (2,2)
+    for (int i = 0; i < TAM_NAVIO; i++)
+    {
+        tabuleiro[i][i] = NAVIO;
+    }
+
+    // Navio diagonal inversa em (0,9) até (2,7)
+    for (int i = 0; i < TAM_NAVIO; i++)
+    {
+        tabuleiro[i][9 - i] = NAVIO;
     }
 
     printf("## Tabuleiro Batalha Naval ##\n");
     // Exibe o cabeçalho com letras A-J
     printf("   ");
-    for (char letra = 'A'; letra <= 'J'; letra++)
+    for (char letra = 'A'; letra < 'A' + TAMANHO; letra++)
     {
         printf("%c ", letra);
     }
     printf("\n");
 
-    // Exibe o tabuleiro
-    for (int i = 0; i < 10; i++)
+    // Exibe o tabuleiro com número da linha
+    for (int i = 0; i < TAMANHO; i++)
     {
         printf("%2d ", i); // Número da linha
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < TAMANHO; j++)
         {
             printf("%d ", tabuleiro[i][j]);
         }
